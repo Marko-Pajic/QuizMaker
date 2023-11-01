f96a408a703dde9b9639a11a8c3881d2188aa51a
Marko-Pajic <pajic.marko84@gmail.com>
2023-11-01 17:37:06 +0100

    fixed #9 make Random object public

--- Logic.cs
+++ Logic.cs
@@ -5,11 +5,10 @@
     {
         public static List<string> IndexShuffle(List<string> answers)
         {
-            Random rng = new Random();
 
             for (int i = answers.Count - 1; i > 0; i--)
             {
-                int j = rng.Next(i + 1);
+                int j = Program.rng.Next(i + 1);
                 string temp = answers[i];
                 answers[i] = answers[j];
                 answers[j] = temp;
