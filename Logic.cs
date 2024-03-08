e24f4ba7b01168d4d71252bb88ab20ade6790a1c
Marko-Pajic <pajic.marko84@gmail.com>
2024-03-09 00:24:29 +0100

    add comments to Logic.cs

--- Logic.cs
+++ Logic.cs
@@ -9,7 +9,7 @@ namespace QuizMaker
         /// using Knuths algorithm to shuffle list of answers
         /// </summary>
         /// <param name="answers"></param>
-        /// <returns>list of answers</returns>
+        /// <returns>shuffled list of answers</returns>
         public static List<string> GetIndexShuffle(List<string> answers)
         {
 
@@ -24,12 +24,14 @@ namespace QuizMaker
             return answers;
         }
         /// <summary>
-        /// 
+        /// 1.shuffles aquired answers
+        /// 2.assigns answers to object
+        /// 3.finds the index of correct answer and assignes it to object property
         /// </summary>
-        /// <param name="question"></param>
-        /// <param name="answers"></param>
-        /// <param name="correctAnswer"></param>
-        /// <returns></returns>
+        /// <param name="question">object containing answers</param>
+        /// <param name="answers">list of answers</param>
+        /// <param name="correctAnswer">string containing correct answer</param>
+        /// <returns>object containing answers and correct answer index</returns>
         public static Question GetAnswerArranged (Question question, List<string> answers, string correctAnswer)
         {
             answers = Logic.GetIndexShuffle(answers);
@@ -40,13 +42,22 @@ namespace QuizMaker
 
             return question;
         }
-
+        /// <summary>
+        /// 1.assigns current list of objects to new list
+        /// 2.serializes object containg list of objects
+        /// </summary>
+        /// <param name="questions">new list of objects</param>
+        /// <param name="quizCategory">object containing list of objects</param>
         public static void AddQuestionAndSerialize (List<Question> questions, QuizCategory quizCategory)
         {
             quizCategory.Questions = questions;
             FileUtils.SerializeCategoryToFile(quizCategory);
         }
-
+        /// <summary>
+        /// 1.instantiates new object
+        /// 2.populate the new object
+        /// </summary>
+        /// <returns>new object populated</returns>
         public static QuizCategory GetCategory()
         {
             QuizCategory quizSection = new QuizCategory();
